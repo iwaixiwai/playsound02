455cc3a26f44cab27a89ea0e93da3098b80788fc
Toshio Iwai <iwai@gol.com>
2025-11-29 12:29:44 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let playButton, stopButton; // プレイとストップのボタン用の変数
 
 // preload関数でサウンドファイルを読み込む
 function preload() {
-  sound = loadSound('Elepla_hanenbow.mp3');
+  sound = loadSound('ElectroplanktonCM.mp3');
 }
 
 // setup関数でキャンバスとUIを作成する
